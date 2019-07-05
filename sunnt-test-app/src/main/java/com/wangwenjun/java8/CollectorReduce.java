a36a86f09abeb4cc7e8f82c1b2e94a8f1780825e
yangzhimeng <yangzhimeng08@163.com>
2019-07-05 16:40:49 +0800

    注释

--- sunnt-test-app/src/main/java/com/wangwenjun/java8/CollectorReduce.java
+++ sunnt-test-app/src/main/java/com/wangwenjun/java8/CollectorReduce.java
@@ -1,15 +1,17 @@
 package com.wangwenjun.java8;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
-/***************************************
- * @author:Alex Wang
- * @Date:2016/10/27 QQ:532500648
- * QQ交流群:286081824
- ***************************************/
+/**
+ * @Description: reduce 可以应用到找最大值、最小值、综合，
+ * (i,j)->{i是上一次结果，比如i+j=下一循环的i}
+ * @Date: 2019/7/5 11:25
+ */
 public class CollectorReduce {
     public static void main(String[] args) {
         List<Dish> menu = Arrays.asList(
@@ -23,26 +25,32 @@ public class CollectorReduce {
                 new Dish("prawns", false, 300, Dish.Type.FISH),
                 new Dish("salmon", false, 450, Dish.Type.FISH));
 
+        /** @desc 根据vegetarian条件过滤，统计数目 */
         long count = menu.stream().filter(d -> d.isVegetarian()).count();
-
+        /** @desc 根据vegetarian条件过滤，统计数目 */
         Long collect = menu.stream().filter(d -> d.isVegetarian()).collect(Collectors.counting());
 
-//        Optional<Integer> maxCalories = menu.stream().map(Dish::getCalories).reduce(Integer::max);
-        Optional<Dish> maxCalories = menu.stream().reduce((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2);
-        maxCalories.ifPresent(System.out::println);
+        /** @desc 获取calories最大值 */
+        Optional<Integer> maxCalories = menu.stream().map(Dish::getCalories).reduce(Integer::min);
 
-        Optional<Dish> maxCaloriesCollect = menu.stream().collect(Collectors.maxBy(Comparator.comparingInt(Dish::getCalories)));
-        maxCaloriesCollect.ifPresent(System.out::println);
-
+        /** @desc //返回最大Dish{name='pork', vegetarian=false, calories=800, type=MEAT} */
+        menu.stream().reduce((dish, dish2) -> dish.getCalories() > dish2.getCalories()
+                ? dish : dish2)
+        // .ifPresent(System.out::println)
+        ;
+        /** @desc //返回最大Dish{name='pork', vegetarian=false, calories=800, type=MEAT} */
+        Optional<Dish> maxCaloriesCollect = menu.stream().max(Comparator.comparingInt(Dish::getCalories));
 
         Integer collect1 = menu.stream().collect(Collectors.collectingAndThen(toList(), t -> t.size()));
 
-
+        /** @desc 根据指定Type分类*/
         Map<Dish.Type, List<Dish>> collect2 = menu.stream().collect(Collectors.groupingBy(Dish::getType));
-
+        System.out.println(collect2);
+        /** @desc 根据指定Type分类,并计算分类的平均calories*/
         Map<Dish.Type, Double> collect3 = menu.stream().collect(Collectors.groupingBy(Dish::getType, Collectors.averagingInt(Dish::getCalories)));
-
-
-
+       
+        /** @desc 根据指定Type分类,并计算分类的最大*/
+        Map<Dish.Type, Optional<Dish>> collect4 = menu.stream().collect(Collectors.groupingBy(Dish::getType, Collectors.maxBy(Comparator.comparing(Dish::getCalories))));
+        System.out.println(collect4);
     }
 }
